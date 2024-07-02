19ef8897243d37d445a8ca5e36f9e1b1b0c7fb1b
Lith <LithWang@outlook.com>
2024-07-02 22:03:40 +0800

    [Vitorm.Data] fix namespace mismacth issue [Vitorm.Data] support  DbType (short name) as provider in settings

--- src/Vitorm.Data/Data.cs
+++ src/Vitorm.Data/Data.cs
@@ -46,16 +46,16 @@ namespace Vitorm
         {
             return providerMap.GetOrAdd(entityType, GetDataProviderFromConfig);
 
-            IDataProvider GetDataProviderFromConfig(Type entityType)
+            static IDataProvider GetDataProviderFromConfig(Type entityType)
             {
-                var Namespace = entityType.Namespace;
-                return providerCache.FirstOrDefault(cache => Namespace.StartsWith(cache.@namespace))?.dataProvider
+                var FullName = entityType.FullName;
+                return providerCache.FirstOrDefault(cache => cache.Match(FullName))?.dataProvider
                     ?? throw new NotImplementedException("can not find config for type: " + entityType.FullName);
             }
         }
-        public static IDataProvider DataProvider(string Namespace)
+        public static IDataProvider DataProvider(string classFullName)
         {
-            return providerCache.FirstOrDefault(cache => Namespace.StartsWith(cache.@namespace))?.dataProvider;
+            return providerCache.FirstOrDefault(cache => cache.Match(classFullName))?.dataProvider;
         }
         public static IDataProvider DataProvider<Entity>() => DataProvider(typeof(Entity));
 
@@ -64,7 +64,24 @@ namespace Vitorm
         {
             public IDataProvider dataProvider;
             public string @namespace;
+            private string classFullNamePrefix;
             public Dictionary<string, object> dataSourceConfig;
+
+            public DataProviderCache(IDataProvider dataProvider, Dictionary<string, object> dataSourceConfig)
+            {
+                this.dataProvider = dataProvider;
+                this.dataSourceConfig = dataSourceConfig;
+
+                if (dataSourceConfig.TryGetValue("namespace", out var ns))
+                {
+                    @namespace = ns as string;
+                    classFullNamePrefix = @namespace + ".";
+                }
+            }
+            internal bool Match(string classFullName)
+            {
+                return classFullName.StartsWith(@namespace);
+            }
         }
 
 
@@ -75,23 +92,36 @@ namespace Vitorm
 
         static DataProviderCache GetDataProvider(Dictionary<string, object> dataSourceConfig)
         {
-            dataSourceConfig.TryGetValue("provider", out var provider);
-            dataSourceConfig.TryGetValue("assemblyName", out var assemblyName);
-            dataSourceConfig.TryGetValue("assemblyFile", out var assemblyFile);
+            /*
+            "provider": "Vitorm.Sqlite.DataProvider",
+            "assemblyName": "Vitorm.Sqlite",
+            "assemblyFile": "Vitorm.Sqlite.dll",
+             */
 
-            Type type = ObjectLoader.GetType(className: dataSourceConfig["provider"] as string, assemblyName: assemblyName as string, assemblyFile: assemblyFile as string);
+            object temp;
+            string provider = dataSourceConfig.TryGetValue("provider", out temp) ? temp as string : null;
+            string assemblyName = dataSourceConfig.TryGetValue("assemblyName", out temp) ? temp as string : null;
+            string assemblyFile = dataSourceConfig.TryGetValue("assemblyFile", out temp) ? temp as string : null;
 
-            var dataProvider = type?.GetConstructor(Type.EmptyTypes)?.Invoke(new object[] { }) as IDataProvider;
+            Type providerType;
+            IDataProvider dataProvider;
+
+            // #1 load
+            providerType = ObjectLoader.GetType(className: provider, assemblyName: assemblyName, assemblyFile: assemblyFile);
+            dataProvider = providerType?.GetConstructor(Type.EmptyTypes)?.Invoke(new object[] { }) as IDataProvider;
+
+            // #2 try load by database type (Sqlite/MySql/SqlServer)
+            if (dataProvider == null)
+            {
+                providerType = ObjectLoader.GetType(className: $"Vitorm.{provider}.DataProvider", assemblyName: $"Vitorm.{provider}", assemblyFile: $"Vitorm.{provider}.dll");
+                dataProvider = providerType?.GetConstructor(Type.EmptyTypes)?.Invoke(new object[] { }) as IDataProvider;
+            }
+
+
             if (dataProvider == null) return null;
 
             dataProvider.Init(dataSourceConfig);
-
-            return new DataProviderCache
-            {
-                dataProvider = dataProvider,
-                @namespace = dataSourceConfig["namespace"] as string,
-                dataSourceConfig = dataSourceConfig,
-            };
+            return new DataProviderCache(dataProvider, dataSourceConfig);
         }
 
 

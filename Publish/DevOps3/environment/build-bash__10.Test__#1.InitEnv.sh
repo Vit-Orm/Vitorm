50900bc7551287ae5eaaebe365c5f529917e670c
Lith <LithWang@outlook.com>
2024-08-24 23:37:25 +0800

    DevOps3

--- Publish/DevOps3/environment/build-bash__10.Test__#1.InitEnv.sh
+++ Publish/DevOps3/environment/build-bash__10.Test__#1.InitEnv.sh
@@ -49,11 +49,11 @@ echo '#build-bash__10.Test__#1.InitEnv.sh -> #8 wait for containers to init'
 
 
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #8.1 wait for MySql to init' 
-docker run -t --rm --link dev-mysql:dev-mysql mysql:8.0.26 timeout 120 sh -c 'until mysql -h dev-mysql -u root -p123456 -e "SELECT 1"; do echo waiting for mysql; sleep 2; done;'
+docker run -t --rm --link dev-mysql:dev-mysql mysql:8.0.26 timeout 120 sh -c 'until mysql -h dev-mysql -u root -p123456 -e "SELECT 1"; do echo waiting for mysql; sleep 2; done;  mysql -h dev-mysql -u root -p123456 -e "create database `dev-orm2`;"'
 
 
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #8.2 wait for SqlServer to init' 
-docker run -t --rm --link dev-mssql:dev-mssql mcr.microsoft.com/mssql/server:2019-CU12-ubuntu-20.04 timeout 120 sh -c 'until /opt/mssql-tools/bin/sqlcmd -S "dev-mssql" -U SA -P "Admin0123" -Q "CREATE DATABASE [dev-orm]"; do echo waiting for mysql; sleep 2; done;'
+docker run -t --rm --link dev-mssql:dev-mssql mcr.microsoft.com/mssql/server:2019-CU12-ubuntu-20.04 timeout 120 sh -c 'until /opt/mssql-tools/bin/sqlcmd -S "dev-mssql" -U SA -P "Admin0123" -Q "SELECT 1"; do echo waiting for mysql; sleep 2; done;  /opt/mssql-tools/bin/sqlcmd -S "dev-mssql" -U SA -P "Admin0123" -Q "CREATE DATABASE [dev-orm]";  /opt/mssql-tools/bin/sqlcmd -S "dev-mssql" -U SA -P "Admin0123" -Q "CREATE DATABASE [dev-orm2]";    '
 
 
 #---------------------------------------------------------------------

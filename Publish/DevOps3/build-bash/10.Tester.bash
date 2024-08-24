13bda8be1437ab85fff47f9064961e2169904f7d
Lith <LithWang@outlook.com>
2024-08-24 13:46:01 +0800

    DevOps3 run test

---
+++ Publish/DevOps3/build-bash/10.Tester.bash
@@ -0,0 +1,65 @@
+set -e
+
+
+#---------------------------------------------------------------------
+# args
+args_="
+
+export basePath=/root/temp/svn
+export NUGET_PATH=$basePath/Publish/release/.nuget
+
+# "
+
+if [ ! $NUGET_PATH ]; then NUGET_PATH=$basePath/Publish/release/.nuget; fi
+
+
+#----------------------------------------------
+echo "#10.Tester.bash -> find test projects and run test"
+
+export devOpsPath="$PWD/.."
+
+docker run -i --rm \
+--env LANG=C.UTF-8 \
+-v $NUGET_PATH:/root/.nuget \
+-v "$basePath":/root/code \
+-v "$basePath":"$basePath" \
+serset/dotnet:sdk-6.0 \
+bash -c "
+set -e
+
+if grep '<test>' -r --include *.csproj /root/code; then
+	echo '#10.Tester.bash -> got projects need to test'
+else
+	echo '#10.Tester.bash -> skip for no project needs to test'
+	exit 0
+fi
+
+
+export basePath=/root/code
+
+echo '#10.Tester.bash -> #2 run test...'
+cd \$basePath
+for file in \$(grep -a '<test>' . -rl --include *.csproj)
+do
+	cd \$basePath
+
+	echo '#10.Tester.bash -> #2 run test:'
+	echo run test for project \"\$file\"
+
+	#publish
+	cd \$(dirname \"\$file\")
+	dotnet test
+
+done
+
+
+"
+
+
+
+echo '#10.Tester.bash -> success!'
+
+
+
+
+
---
+++ Publish/DevOps3/environment/README-DevOps.md
@@ -0,0 +1,20 @@
+
+# DevOps 3.5
+
+
+# build-bash
+extra steps when building, all sh files could be remove if not needed.
+
+
+
+
+----------------------------------------------
+# ReleaseLog
+
+-----------------------
+# 3.5
+> 2024-08-24
+
+- support tester
+
+

ae6c8079b9b9405811b2772886f5f9d804024998
Lith <LithWang@outlook.com>
2024-08-24 18:37:14 +0800

    DevOps

---
+++ Publish/DevOps3/environment/__build-bash__40.Station-publish__#4_copyExtraReleaseFiles.sh
@@ -0,0 +1,21 @@
+set -e
+
+#---------------------------------------------------------------------
+# args
+args_="
+
+export basePath=/root/temp
+export netVersion=net6.0
+export publishPath=\"$basePath/Publish/release/release/Station($netVersion)\"
+
+# "
+
+
+
+#----------------------------------------------
+echo '#build-bash__40.Station-publish__#4_copyExtraReleaseFiles.sh'
+
+
+
+
+

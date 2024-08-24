00433d42c76c2eceb4f492ebf2cc078d13895786
Lith <LithWang@outlook.com>
2024-08-24 23:42:48 +0800

    DevOps

--- Publish/DevOps3/environment/build-bash__10.Test__#1.InitEnv.sh
+++ Publish/DevOps3/environment/build-bash__10.Test__#1.InitEnv.sh
@@ -49,7 +49,7 @@ echo '#build-bash__10.Test__#1.InitEnv.sh -> #8 wait for containers to init'
 
 
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #8.1 wait for MySql to init' 
-docker run -t --rm --link dev-mysql:dev-mysql mysql:8.0.26 timeout 120 sh -c 'until mysql -h dev-mysql -u root -p123456 -e "SELECT 1"; do echo waiting for mysql; sleep 2; done;  mysql -h dev-mysql -u root -p123456 -e "create database `dev-orm2`;"'
+docker run -t --rm --link dev-mysql:dev-mysql mysql:8.0.26 timeout 120 sh -c 'until mysql -h dev-mysql -u root -p123456 -e "SELECT 1"; do echo waiting for mysql; sleep 2; done;  mysql -h dev-mysql -u root -p123456 -e "create database \`dev-orm2\`"'
 
 
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #8.2 wait for SqlServer to init' 
